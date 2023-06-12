5b769d24168202c302ae084b87aaef33750515d8
marcmarb <fotsmacc@gmail.com>
2023-06-12 16:26:41 -0600

    Initial Commit

---
+++ Web02/Web02App.swift
@@ -0,0 +1,17 @@
+//
+//  Web02App.swift
+//  Web02
+//
+//  Created by Marcos on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Web02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
